88f2d0ab78f8454702bdfddb2f6c911118443dcf
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-06-27 19:00:50 -0300

    Criando uma função que Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -85,19 +85,23 @@ function fizzBuzz(array) {
   }
   return newArray;
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-// ['bug!', 'fizzBuzz', 'bug!', 'fizz', 'fizzBuzz']
-console.log(fizzBuzz([7, 9]));
-// ['bug!', 'fizz']
-console.log(fizzBuzz([9, 25]));
-// ['fizz', 'buzz']
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(palavra) {
+  palavra = palavra.replace(/a/g, 1);
+  palavra = palavra.replace(/e/g, 2);
+  palavra = palavra.replace(/i/g, 3);
+  palavra = palavra.replace(/o/g, 4);
+  palavra = palavra.replace(/u/g, 5);
+  return palavra;
 }
-function decode() {
-  // seu código aqui
+function decode(palavra) {
+  palavra = palavra.replace(/1/g, 'a');
+  palavra = palavra.replace(/2/g, 'e');
+  palavra = palavra.replace(/3/g, 'i');
+  palavra = palavra.replace(/4/g, 'o');
+  palavra = palavra.replace(/5/g, 'u');
+  return palavra;
 }
 
 // Desafio 10
